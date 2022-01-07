da07ca77e5861ebcba338884c4e59e1129d71399
Jerry Zhong <zhong20000905@qq.com>
2022-01-07 23:44:17 +0800

    back up

--- src/main/java/io/github/jerryzhongj/calabash_brothers/Game.java
+++ src/main/java/io/github/jerryzhongj/calabash_brothers/Game.java
@@ -12,6 +12,8 @@ import javax.swing.event.AncestorEvent;
 import org.xml.sax.InputSource;
 
 import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.DoubleBinding;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ListProperty;
@@ -20,6 +22,8 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleMapProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.MapChangeListener;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
@@ -32,7 +36,8 @@ class Game{
     @Getter
     private Scene scene = new Scene(canvas);
     private MapProperty<Integer, Element> elements = new SimpleMapProperty<>();
-    private Element anchoredElement;
+    private DoubleProperty anchoredX = new SimpleDoubleProperty();
+    private DoubleProperty anchoredY = new SimpleDoubleProperty();
     private DoubleProperty camX = new SimpleDoubleProperty();
     private DoubleProperty camY = new SimpleDoubleProperty();
     private DoubleProperty width = new SimpleDoubleProperty(GlobalSettings.PREF_WIDTH);
@@ -41,11 +46,61 @@ class Game{
         canvas.setPrefSize(GlobalSettings.PREF_WIDTH, GlobalSettings.PREF_HEIGHT);
         canvas.maxHeightProperty().bind(height);
         canvas.maxWidthProperty().bind(width);
-    }
 
-    void computeCameraPostion(){
-        // TODO
-        
+        // Setting Camera
+        DoubleBinding camXBindings = Bindings.createDoubleBinding(()->{
+            double ax = anchoredX.get();
+            double cx = camX.get();
+            double canvasWidth = canvas.widthProperty().get();
+            double relative = (ax - cx) / canvasWidth;
+            double w = width.get();
+            // Place the character in the middle
+            if(relative < 0.25)
+                cx = ax - 0.25 * canvasWidth;
+            if(relative > 0.75)
+                cx = ax - 0.75 * canvasWidth;
+            // 
+            if(cx < -w / 2)
+                cx = -w / 2;
+            if(cx > w / 2 - canvasWidth)
+                cx = w / 2 - canvasWidth;
+
+            return cx;
+        }, anchoredX, canvas.widthProperty(), width);
+
+        DoubleBinding camYBindings = Bindings.createDoubleBinding(()->{
+            double ay = anchoredY.get();
+            double cy = camY.get();
+            double canvasHeight = canvas.heightProperty().get();
+            double relative = (cy - ay) / canvasHeight;
+            double h = height.get();
+            if(relative < 0.25)
+                cy = ay + 0.25 * canvasHeight;
+            if(relative > 0.75)
+                cy = ay + 0.75 * canvasHeight;
+
+            if(cy < canvasHeight)
+                cy = canvasHeight;
+            if(cy > h)
+                cy = h;
+
+            return cy;
+        }, anchoredY, canvas.heightProperty(), height);
+
+        camX.bind(camXBindings);
+        camY.bind(camYBindings);
+
+        elements.addListener((MapChangeListener.Change<? extends Integer, ? extends Element> change) ->{
+                if(change.wasAdded()){
+                    
+                }
+
+                if(change.wasRemoved()){
+                    
+                }
+                
+            }
+        );
     }
 
     void connect(String serverName, String name, byte CalabashType) throws UnknownHostException, IOException{
@@ -74,6 +129,7 @@ class Game{
         DoubleProperty y = new SimpleDoubleProperty(0);
         BooleanProperty reverse = new SimpleBooleanProperty(false);
         byte elementType;
+        Node node;
         Element(byte type){
             elementType = type;
         }
@@ -162,8 +218,12 @@ class Game{
                 case ResponseProtocol.ANCHOR:
                     id = inputStream.readInt();
                     e = elements.get(id);
-                    if(e != null)
-                        anchoredElement = e;
+                    if(e != null){
+                        Platform.runLater(()->{
+                            anchoredX.bind(e.x);
+                            anchoredY.bind(e.y);
+                        });
+                    }
                     break;
                 case ResponseProtocol.SET_SIZE:
                     id = inputStream.readInt();
